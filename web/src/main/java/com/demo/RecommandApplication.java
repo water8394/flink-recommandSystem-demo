a3f3ea02b00796a68d4903199f63d39b236b73f7
xinze <1050748528@qq.com>
2019-05-23 22:21:36 +0800

    模块 -> 初始化web模块

---
+++ web/src/main/java/com/demo/RecommandApplication.java
@@ -0,0 +1,15 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author XINZE
+ */
+@SpringBootApplication
+public class RecommandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecommandApplication.class);
+    }
+}
