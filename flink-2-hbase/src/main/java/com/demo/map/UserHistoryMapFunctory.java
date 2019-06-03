754b0636b857ef920edd614f4c728cbc431dbf92
xinze <1050748528@qq.com>
2019-06-03 12:32:07 +0800

    功能 -> 增加商品-用户表

--- flink-2-hbase/src/main/java/com/demo/map/UserHistoryMapFunctory.java
+++ flink-2-hbase/src/main/java/com/demo/map/UserHistoryMapFunctory.java
@@ -10,13 +10,18 @@ import org.apache.flink.api.common.functions.MapFunction;
  */
 public class UserHistoryMapFunctory implements MapFunction<String, String> {
 
-
+    /**
+     * 将 用户-产品  和 产品-用户 分别存储Hbase表
+     * @param s
+     * @return
+     * @throws Exception
+     */
     @Override
     public String map(String s) throws Exception {
         LogEntity log = LogToEntity.getLog(s);
         if (null != log){
-            String rowKey = log.getUserId() + "";
-            HbaseClient.increamColumn("history",rowKey,"p",String.valueOf(log.getProductId()));
+            HbaseClient.increamColumn("u_history",String.valueOf(log.getUserId()),"p",String.valueOf(log.getProductId()));
+            HbaseClient.increamColumn("p_history",String.valueOf(log.getProductId()),"p",String.valueOf(log.getUserId()));
         }
         return "";
     }
