74a6d76acd21b2a05123a2733af2749e3f6c0c5c
xinze <1050748528@qq.com>
2019-06-18 21:22:56 +0800

    功能 -> 增加每隔小时的定时调度任务，分别取计算基于协同过滤的产品相似度评分和基于标签的产品相似度评分

---
+++ flink-2-hbase/src/main/java/com/demo/scheduler/SshedulerJob.java
@@ -0,0 +1,45 @@
+package com.demo.scheduler;
+
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class SshedulerJob {
+
+    /**
+     * 每12小时定时调度一次 基于两个推荐策略的 产品评分计算
+     * 策略1 ：协同过滤
+     *           abs( i ∩ j)
+     *      w = ——————————————
+     *           sqrt(i || j)
+     *
+     *
+     * 策略2 ： 基于产品标签 计算产品的余弦相似度
+     *
+     *     w = sqrt(  pow((tag{i,a} - tag{j,a}),2)  + pow((tag{i,b} - tag{j,b}),2)      )
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        ScheduledExecutorService pool = new ScheduledThreadPoolExecutor(2);
+        // 每12小时调度一次
+        pool.scheduleAtFixedRate(new Task(), 0, 12, TimeUnit.HOURS);
+
+    }
+
+    private static class Task implements Runnable{
+        ItemCfCoeff item = new ItemCfCoeff();
+        ProductCoeff prod = new ProductCoeff();
+        @Override
+        public void run() {
+            try {
+                item.getSingelItemCfCoeff("1",new String[]{"2","3"});
+                prod.getSingelProductCoeff("1",new String[]{"2","3"});
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+}
