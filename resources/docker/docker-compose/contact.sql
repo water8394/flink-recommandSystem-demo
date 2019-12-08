d9899bf6e9e9381232d9e6122331d757ef541fd3
xinze <1050748528@qq.com>
2019-12-08 23:35:44 +0800

    debug -> 修复sql文件

--- resources/docker/docker-compose/contact.sql
+++ resources/docker/docker-compose/contact.sql
@@ -17,7 +17,7 @@
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
-CREATE DATABASE IF EXISTS con;
+CREATE DATABASE IF NOT EXISTS con;
 USE con;
 -- ----------------------------
 -- Table structure for contact
